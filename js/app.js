e0c6d62defc632295ee2e85b51c58c3b49940552
Sander Steensma <sander.v.m.steensma@gmail.com>
2022-05-31 00:59:06 +0200

    ver 0.1

--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
 var menubutton = document.querySelector('.menubutton');
 var header = document.querySelector('header');
 
-// console.log('Hello World!');
 
-// menubutton.addEventListener('click', () => {
-//     switch(header.classList.contains('hide')) {
-//         case true:
-//             header.classList.remove('hide');
-//             break;
-//         case false:
-//             header.classList.add('hide');
-//             break;
-//     }
-// });
+menubutton.addEventListener('click', toggleVisible);
 
 function toggleVisible() {
     menubutton.classList.toggle('active')
